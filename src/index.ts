b26b7623a5e5b428ef429aa35fc08b67f0438bdb
Harrison Parkes <hpa101@uclive.ac.nz>
2025-04-30 11:07:16 +1200

    Chore: Updated npm packages and renamed from EXAMPLE to swimdsl

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
+import { parser } from "./syntax.grammar"
+import { LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent } from "@codemirror/language"
+import { styleTags, tags as t } from "@lezer/highlight"
 
-export const EXAMPLELanguage = LRLanguage.define({
+export const SwimdslLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
+        Application: delimitedIndent({ closing: ")", align: false })
       }),
       foldNodeProp.add({
         Application: foldInside
@@ -21,10 +21,10 @@ export const EXAMPLELanguage = LRLanguage.define({
     ]
   }),
   languageData: {
-    commentTokens: {line: ";"}
+    commentTokens: { line: ";" }
   }
 })
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export function swimdsl() {
+  return new LanguageSupport(SwimdslLanguage)
 }
