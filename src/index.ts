aca00c20ac27170b247da7d5e563bf0e5e33f979
Harrison Parkes <git@hparkes.com>
2025-04-30 20:16:53 +1200

    Feature: Failed attempt to add some autocompletions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { completeFromList } from "@codemirror/autocomplete";
 import { LRLanguage, LanguageSupport, delimitedIndent, foldInside, foldNodeProp, indentNodeProp } from "@codemirror/language";
 import { styleTags, tags } from "@lezer/highlight";
 import { LRParser } from "@lezer/lr";
 import { parser } from "./syntax.grammar";
+import { Extension } from "@uiw/react-codemirror";
 
 let parserWithMetadata: LRParser = parser.configure({
   props: [
@@ -29,6 +31,15 @@ export const swimdslLanguage: LRLanguage = LRLanguage.define({
   },
 });
 
+export const swimdslCompletion: Extension = swimdslLanguage.data.of({
+  autocomplete: completeFromList([
+    { label: "Freestyle", type: "enum" },
+    { label: "Backstroke", type: "enum" },
+    { label: "Breaststroke", type: "enum" },
+    { label: "Butterfly", type: "enum" },
+  ])
+});
+
 export function swimdsl(): LanguageSupport {
-  return new LanguageSupport(swimdslLanguage);
+  return new LanguageSupport(swimdslLanguage, [swimdslCompletion]);
 }
