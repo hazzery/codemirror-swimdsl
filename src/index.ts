f7dcacdd05e512b420998bae14ea3cf54107657d
Harrison Parkes <hpa101@uclive.ac.nz>
2025-05-04 11:00:08 +1200

    Feature + Refactor: Implemented first lint rule with fix action (undefined pace name) + moved linting and autocompletion code into separate files

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
-import { Completion, CompletionContext } from "@codemirror/autocomplete";
-import { LRLanguage, LanguageSupport, delimitedIndent, foldInside, foldNodeProp, indentNodeProp, syntaxTree } from "@codemirror/language";
-import { Diagnostic, linter } from "@codemirror/lint";
-import { EditorView } from "@codemirror/view";
+import { LRLanguage, LanguageSupport, delimitedIndent, foldInside, foldNodeProp, indentNodeProp } from "@codemirror/language";
 import { styleTags, tags } from "@lezer/highlight";
 import { LRParser } from "@lezer/lr";
-import { parser } from "./syntax.grammar";
 
-const strokeList: Completion[] = [
-  { label: "Freestyle", type: "constant" },
-  { label: "Backstroke", type: "constant" },
-  { label: "Breaststroke", type: "constant" },
-  { label: "Butterfly", type: "constant" }
-];
+import completeSwimDsl from "./completion";
+import swimdslLinter from "./lint";
+import { parser } from "./syntax.grammar";
 
 let parserWithMetadata: LRParser = parser.configure({
   props: [
@@ -40,63 +33,9 @@ export const swimdslLanguage: LRLanguage = LRLanguage.define({
   },
 });
 
-function lintSwimDsl(view: EditorView) {
-  let diagnostics: Diagnostic[] = [];
-
-  syntaxTree(view.state).cursor().iterate(node => {
-    if (node.name == "Identifier") {
-      diagnostics.push({
-        from: node.from,
-        to: node.to,
-        severity: "error",
-        message: "This is an Identifier",
-      });
-    }
-  });
-
-  return diagnostics;
-}
-
-function completeSwimDSL(context: CompletionContext) {
-  let nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1);
-  // You can examine the node type, its parent, or even the node text to decide what completions to offer
-  if (nodeBefore.name === "Stroke" || nodeBefore.parent?.name === "Stroke") {
-    // If the cursor is inside or right after a Stroke field, suggest stroke names.
-    return {
-      from: context.pos,
-      options: strokeList,
-      validFor: /^[A-Za-z]/
-    };
-  }
-  // For pace keyword completion at the start of a line (e.g., when writing a PaceDefinition)
-  if (nodeBefore.name === "Statement" &&
-    context.state.doc.lineAt(context.pos).text.trim().startsWith("P")) {
-    return {
-      from: context.pos,
-      options: [{ label: "Pace", type: "keyword", boost: 90 }],
-      validFor: /^Pace?/i
-    };
-  }
-  // // If the previous token was the "@" symbol, offer pace alias completions.
-  // // One way is to check the text immediately before the cursor.
-  // let line = context.state.doc.lineAt(context.pos);
-  // let beforeCursor = line.text.slice(0, context.pos - line.from);
-  // if (/@\s/.test(beforeCursor)) {
-  //   return {
-  //     from: context.pos,
-  //     options: paceAliasList,
-  //     validFor: /^[A-Za-z]*/
-  //   };
-  // }
-  return null;
-}
-
 export const swimdslCompletion = swimdslLanguage.data.of({
-  autocomplete: completeSwimDSL
+  autocomplete: completeSwimDsl
 });
-
-export const swimdslLinter = linter(lintSwimDsl);
-
 
 export function swimdsl(): LanguageSupport {
   return new LanguageSupport(swimdslLanguage, [swimdslCompletion, swimdslLinter]);
