8968766b7086eb51cead0b969c4685815cba2572
Harrison Parkes <hpa101@uclive.ac.nz>
2025-05-04 12:26:08 +1200

    Feature: Implemented lint rule for syntax errors

--- src/lint.ts
+++ src/lint.ts
@@ -1,6 +1,59 @@
 import { syntaxTree } from '@codemirror/language';
 import { linter, Diagnostic } from '@codemirror/lint';
+import { EditorState } from '@codemirror/state';
 import { EditorView } from '@codemirror/view';
+import { SyntaxNodeRef, TreeCursor } from '@lezer/common';
+
+function undefinedPaceNameMessage(pace_name: string): string {
+  return `'${pace_name}' is not a defined pace name.
+If you wish to be able to use '${pace_name}' in the place of a pace percentage, please define it with the following line:
+Pace ${pace_name} = _%`;
+}
+
+function lintUndefinedPaceName(
+  node: SyntaxNodeRef,
+  declaredIdentifiers: Set<string>,
+  state: EditorState,
+  diagnostics: Diagnostic[],
+) {
+  if (node.name !== "Identifier") return;
+
+  let parent = node.node.parent;
+  if (parent === null) return;
+
+  // Ask the document for the characters that make up the Identifier node
+  let node_value = state.sliceDoc(node.from, node.to);
+
+  if (parent.name === "PaceDefinition") {
+    declaredIdentifiers.add(node_value);
+  } else if (parent.name === "PaceAlias" && !declaredIdentifiers.has(node_value)) {
+    diagnostics.push({
+      from: node.from,
+      to: node.to,
+      severity: "error",
+      message: undefinedPaceNameMessage(node_value),
+      actions: [{
+        name: "Define pace name",
+        apply(view: EditorView, _from: number, _to: number) {
+          view.dispatch(
+            { changes: { from: 0, to: 0, insert: `Pace ${node_value} = _%\n` } },
+          );
+        }
+      }]
+    });
+  }
+}
+
+function lintSyntaxErrors(node: SyntaxNodeRef, diagnostics: Diagnostic[]) {
+  if (node.name !== "⚠") return;
+
+  diagnostics.push({
+    from: node.from,
+    to: node.to,
+    severity: "error",
+    message: "Syntax error"
+  });
+}
 
 function swimdslLintSource(view: EditorView): Diagnostic[] {
   const diagnostics: Diagnostic[] = [];
@@ -8,33 +61,14 @@ function swimdslLintSource(view: EditorView): Diagnostic[] {
   const declaredIdentifiers = new Set<string>();
 
   let state = view.state;
-  syntaxTree(state).cursor().iterate(node => {
-    if (node.name !== "Identifier") return;
+  let treeCursor: TreeCursor = syntaxTree(state).cursor();
 
-    let parent = node.node.parent;
-    if (parent === null) return;
-
-    let node_value = state.sliceDoc(node.from, node.to);
-    if (parent.name === "PaceDefinition") {
-      declaredIdentifiers.add(node_value);
-    } else if (parent.name === "PaceAlias" && !declaredIdentifiers.has(node_value)) {
-      diagnostics.push({
-        from: node.from,
-        to: node.to,
-        severity: "error",
-        message: `This pace name has not been defined. If you wish to be able to use this name in the place of a pace specifier please define it with the following line: Pace ${node_value} = _%`,
-        actions: [{
-          name: "Define pace name",
-          apply(view, _from, _to) {
-            view.dispatch({ changes: { from: 0, to: 0, insert: `Pace ${node_value} = _%\n` } });
-          }
-        }]
-      });
-    }
-  });
+  while (treeCursor.next()) {
+    lintUndefinedPaceName(treeCursor, declaredIdentifiers, state, diagnostics);
+    lintSyntaxErrors(treeCursor, diagnostics);
+  }
 
   return diagnostics;
 }
 
 export default linter(swimdslLintSource);
-
