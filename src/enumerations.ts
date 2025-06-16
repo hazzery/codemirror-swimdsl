55320eed822b5b9c83877ead99c064741249e101
Harrison Parkes <harrison@hparkes.com>
2025-06-16 18:16:01 +1200

    Feature: Added Choise, Medley, and NumberX strokes

--- src/enumerations.ts
+++ src/enumerations.ts
@@ -11,8 +11,16 @@ export const strokeNames: string[] = [
   "Butterfly",
   "Fly",
   "Fl",
+  "Choice",
+  "IndividualMedley",
+  "Medley",
+  "MedleyOrder",
+  "NumberOne",
+  "NumberTwo",
+  "NumberThree",
+  "NumberFour",
 ];
-export const strokeTypes: string[] = ["Pull", "Kick"];
+export const strokeTypes: string[] = ["Pull", "Kick", "Drill"];
 export const lengthUnits: string[] = ["meters", "metres", "yards"];
 export const distanceUnits: string[] = ["meters", "metres", "yards", "Lengths"];
-export const requiredGear: string[] = ["Paddle", "Fins", "Bouy", "Board"];
+export const requiredGear: string[] = ["Paddles", "Fins", "Bouy", "Board"];
