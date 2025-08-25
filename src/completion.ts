a77cc0f3b513b3b9becc67e3576e7b1a792d96ca
Harrison Parkes <git@hparkes.com>
2025-08-25 13:58:07 +1200

    Refactor: Replaced nodeCompletions array with map
    This make code more readable and self documenting.

--- src/completion.ts
+++ src/completion.ts
@@ -8,6 +8,20 @@ import { syntaxTree } from "@codemirror/language";
 import { requiredGear, strokeNames, strokeTypes } from "./enumerations";
 import { definedIdentifiersField } from "./definedIdentifiers";
 
+const enum CompletableNodes {
+  STROKE_NAME,
+  GEAR_NAME,
+  PACE_ALIAS,
+  STROKE_MODIFIER,
+  NUMBER_COMPLETEABLE_NODES,
+}
+
+interface NodeCompletionSpec {
+  priorNodeName: string;
+  nodeName: string;
+  completions: Completion[];
+}
+
 // Convert all stroke names to autocomplpetions.
 const strokeNameCompletions: Completion[] = strokeNames.map((strokeName) => ({
   label: strokeName,
@@ -24,12 +38,40 @@ const strokeModifierCompletions: Completion[] = strokeTypes.map(
   (strokeModifier) => ({ label: strokeModifier, type: "constant" }),
 );
 
-const nodeCompletioons: [string, string, Completion[]][] = [
-  ["Distance", "Stroke", strokeNameCompletions],
-  ["GearSpecification", "RequiredGear", gearNameCompletions],
-  ["Pace", "PaceAlias", []],
-  ["", "StrokeType", strokeModifierCompletions],
-];
+const nodeCompletions: Map<CompletableNodes, NodeCompletionSpec> = new Map([
+  [
+    CompletableNodes.STROKE_NAME,
+    {
+      priorNodeName: "Distance",
+      nodeName: "Stroke",
+      completions: strokeNameCompletions,
+    },
+  ],
+  [
+    CompletableNodes.GEAR_NAME,
+    {
+      priorNodeName: "GearSpecification",
+      nodeName: "RequiredGear",
+      completions: gearNameCompletions,
+    },
+  ],
+  [
+    CompletableNodes.PACE_ALIAS,
+    {
+      priorNodeName: "Pace",
+      nodeName: "PaceAlias",
+      completions: [] as Completion[],
+    },
+  ],
+  [
+    CompletableNodes.STROKE_MODIFIER,
+    {
+      priorNodeName: "",
+      nodeName: "StrokeType",
+      completions: strokeModifierCompletions,
+    },
+  ],
+]);
 
 /**
  * Provide the user with autocomplpetions within the editor based on the current
@@ -44,12 +86,15 @@ function completeSwimDSL(context: CompletionContext): CompletionResult | null {
   const nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1);
 
   // Fetch the list of all defined pace names, and convert to autocomplpetions.
-  nodeCompletioons[2][2] = Array.from(
+  nodeCompletions.get(CompletableNodes.PACE_ALIAS)!.completions = Array.from(
     context.state.field(definedIdentifiersField),
   ).map((paceName) => ({ label: paceName, type: "variable" }));
 
-
-  for (const [priorNodeName, nodeName, completions] of nodeCompletioons) {
+  for (const {
+    priorNodeName,
+    nodeName,
+    completions,
+  } of nodeCompletions.values()) {
     // If the user has just typed a character placing the curson in a position
     // which accepts autocomplpetions, provide the autocomplpetions.
     if (nodeBefore.name === priorNodeName) {
