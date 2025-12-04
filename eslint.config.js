a2a9f275b892c1cf26397c88591c4c1b214e3864
Harrison Parkes <git@hparkes.com>
2025-12-05 06:58:29 +1000

    Updated ESLin config to use new defineConfig function

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,33 @@
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config({
-  extends: [
-    js.configs.recommended,
-    tseslint.configs.strictTypeChecked,
-    tseslint.configs.stylisticTypeChecked,
-  ],
-  files: ["**/*.{ts}"],
-  ignores: ["dist", "codemirror-swimdsl/dist"],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+/** @type {import('eslint').Linter.Config[]} */
+export default defineConfig([
+  {
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ["**/*.{ts}"],
+    ignores: ["dist"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-});
+]);
