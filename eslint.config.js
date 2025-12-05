0d7de790a12faf2dfe838897dcdba153605dfbd7
Harrison Parkes <git@hparkes.com>
2025-12-06 07:31:42 +1000

    Fix: Corrected ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -6,21 +6,15 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default defineConfig([
   {
+    ignores: ["dist"],
+  },
+  {
+    files: ["**/*.ts"],
     extends: [
       js.configs.recommended,
       tseslint.configs.strictTypeChecked,
       tseslint.configs.stylisticTypeChecked,
     ],
-    files: ["**/*.{ts}"],
-    ignores: ["dist"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
