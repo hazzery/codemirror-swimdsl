2ad0b1eae9155cb1e45851e898574de4bf29047b
Harrison Parkes <git@hparkes.com>
2025-12-07 08:57:09 +1000

    Chore: Upgraded Rollup config filt to be TypeScript

---
+++ rollup.config.ts
@@ -0,0 +1,20 @@
+import typescript from "@rollup/plugin-typescript";
+import type { RollupOptions } from "rollup";
+import { lezer } from "@lezer/generator/rollup";
+
+const config: RollupOptions = {
+  input: "src/index.ts",
+  external: (id: string) => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
+  output: [
+    { file: "dist/index.cjs", format: "cjs" },
+    { dir: "./dist", format: "es" },
+  ],
+  plugins: [
+    lezer(),
+    typescript({
+      declarationDir: "./dist",
+    }),
+  ],
+};
+
+export default config;
