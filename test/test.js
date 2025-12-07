0b484cdcef9ba5fbbc89762326fcbb60ca2f9da2
Harrison Parkes <git@hparkes.com>
2025-12-07 11:15:33 +1000

    Test: Created test cases for the Lezer grammar

--- test/test.js
+++ test/test.js
@@ -1,17 +1,21 @@
-import { swimdslLanguage } from "../dist/index.js"
-import { fileTests } from "@lezer/generator/dist/test"
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
 
-import * as fs from "fs"
-import * as path from "path"
-import { fileURLToPath } from 'url';
-let caseDir = path.dirname(fileURLToPath(import.meta.url))
+import { fileTests } from "@lezer/generator/dist/test";
+import { swimdslLanguage } from "../dist/index.js";
+
+let caseDir = path.dirname(fileURLToPath(import.meta.url));
 
 for (let file of fs.readdirSync(caseDir)) {
-  if (!/\.txt$/.test(file)) continue
+  if (!/\.lezertest$/.test(file)) continue;
 
-  let name = /^[^\.]*/.exec(file)[0]
+  let name = /^[^\.]*/.exec(file)[0];
   describe(name, () => {
-    for (let { name, run } of fileTests(fs.readFileSync(path.join(caseDir, file), "utf8"), file))
-      it(name, () => run(swimdslLanguage.parser))
-  })
+    for (let { name, run } of fileTests(
+      fs.readFileSync(path.join(caseDir, file), "utf8"),
+      file,
+    ))
+      it(name, () => run(swimdslLanguage.parser));
+  });
 }
