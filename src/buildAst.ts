732da80bd0d473a2033e77a721b7b20103820594
hypercandy <amielemiguel@gmail.com>
2026-05-14 22:25:15 +1200

    Exclude align (#43)
    Co-authored-by: Harrison Parkes <git@hparkes.com>

--- src/buildAst.ts
+++ src/buildAst.ts
@@ -254,6 +254,10 @@ function visitInstructionModifier(
     return visitPace(cursor, state);
   }
 
+  if (cursor.name === "ExcludeAlignSpecification") {
+    return { modifier: InstructionModifiers.EXCLUDE_ALIGN };
+  }
+
   if (cursor.name === "Breathe") {
     return visitBreathe(cursor, state);
   }
